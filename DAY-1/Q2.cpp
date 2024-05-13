40396e8d4473e93af66f448e7a62650daced1996
exoticuday <159710194+exoticuday@users.noreply.github.com>
2024-05-13 19:09:00 +0530

    Update Q2.cpp

--- DAY-1/Q2.cpp
+++ DAY-1/Q2.cpp
@@ -1,1 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
 
+void reversearray(int arr[], int size){
+    int N = size;
+    for(int i=0; i<N/2 ; i++){
+        int temp = arr[i];
+        arr[i]= arr[N-i-1];
+        arr[N-i-1]= temp;
+    }
+}
+
+int main(){
+    int arr[5]= {1,2,3,4,5};
+    reversearray(arr,5);
+    for(int j=0; j<5 ; j++){
+        cout << arr[j]<< endl;
+    }
+}
