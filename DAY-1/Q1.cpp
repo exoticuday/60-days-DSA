b6eb36856514a3f2fe9c8319beb346ed3fe4ff27
exoticuday <159710194+exoticuday@users.noreply.github.com>
2024-06-04 22:56:35 +0530

    Update Q1.cpp

--- DAY-1/Q1.cpp
+++ DAY-1/Q1.cpp
@@ -1,1 +1,17 @@
+# include <bits/stdc++.h>
+using namespace std;
 
+int main(){
+    int n;
+    cin >> n;
+    vector<int> v;
+    for (int i=0;i<n;i++){
+        int x;
+        cin >> x;
+        v.push_back(x);
+    }
+    sort(v.begin(),v.end());
+    cout << "The minimum element is "<< v.front() << endl;
+    cout << "The maximum element is "<< v.back() << endl;
+
+}
